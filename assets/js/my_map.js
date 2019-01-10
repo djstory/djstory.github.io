4f169b76ef80bed2a23437892de18a4e1c687c83
Daniel Story <daniel@danieljstory.com>
2019-01-09 20:49:42 -0500

    updating

--- assets/js/my_map.js
+++ assets/js/my_map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsanN0b3J5IiwiYSI6ImNpbTZnbzNqbDAyNzd0e
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/danieljstory/cjqpqu7160add2qn4w3ahyxks', // stylesheet location
-    center: [-33.547704,27.789823], // starting position [lng, lat]
+    center: [-6.527360,37.266810], // starting position [lng, lat]
     zoom: 0.3 // starting zoom
 });
 
