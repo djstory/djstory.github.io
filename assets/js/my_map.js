463b8ab0fe31aafffaf5b7a09a680a36655b45da
Daniel Story <daniel@danieljstory.com>
2019-01-09 20:53:54 -0500

    updating

--- assets/js/my_map.js
+++ assets/js/my_map.js
@@ -52,4 +52,71 @@ map.on('load', function() {
       'circle-color': 'rgb(244, 235, 66)'
       }
     });
+
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+  // Popup for Barnum and Bailey
+    map.on('mouseenter', 'Barnum and Bailey', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Get the information to display
+        var info = "<p id=\"city\">" + e.features[0].properties.city + "</p><p id=\"state\">" + e.features[0].properties.state + "</p><p id=\"country\">" + e.features[0].properties.country + "<p id=\"circus\">Barnum and Bailey</p>";
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(e.features[0].geometry.coordinates)
+            .setHTML(info)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'Barnum and Bailey', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+
+
+    // Popup for Buffal Bill
+    map.on('mouseenter', 'Buffalo Bill', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Get the information to display
+        var info = "<p id=\"city\">" + e.features[0].properties.city + "</p><p id=\"state\">" + e.features[0].properties.state + "</p><p id=\"country\">" + e.features[0].properties.country + "<p id=\"circus\">Buffalo Bill</p>";
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(e.features[0].geometry.coordinates)
+            .setHTML(info)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'Buffalo Bill', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+
+    // Popup for Cooper Bailey
+    map.on('mouseenter', 'Cooper Bailey', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Get the information to display
+        var info = "<p id=\"city\">" + e.features[0].properties.city + "</p><p id=\"state\">" + e.features[0].properties.state + "</p><p id=\"country\">" + e.features[0].properties.country + "<p id=\"circus\">Cooper Bailey</p>";
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(e.features[0].geometry.coordinates)
+            .setHTML(info)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'Cooper Bailey', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
 });
