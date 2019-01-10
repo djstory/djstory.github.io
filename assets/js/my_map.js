95aefa5cde7a510e9f57dbe8e8f6ab2edb352821
Daniel Story <daniel@danieljstory.com>
2019-01-09 20:39:34 -0500

    updating

--- assets/js/my_map.js
+++ assets/js/my_map.js
@@ -22,4 +22,34 @@ map.on('load', function() {
       'circle-color': 'rgb(124, 218, 255)'
     }
     });
-});
+  });
+
+  map.addLayer({
+      id: 'Buffalo Bill',
+      type: 'circle',
+      source: {
+        type: 'vector',
+        url: 'mapbox://danieljstory.6kgyb3tc'
+      },
+      'source-layer': 'buffalo-bill-master-9ju0dx',
+      paint: {
+      'circle-radius': 4,
+      'circle-opacity': 0.2,
+      'circle-color': 'rgb(255, 142, 238)'
+    }
+    });
+    
+    map.addLayer({
+      id: 'Cooper Bailey',
+      type: 'circle',
+      source: {
+        type: 'vector',
+        url: 'mapbox://danieljstory.21cvqmhe'
+      },
+      'source-layer': 'cooper-bailey-master-53k5wd',
+      paint: {
+      'circle-radius': 4,
+      'circle-opacity': 0.2,
+      'circle-color': 'rgb(244, 235, 66)'
+    }
+    });
