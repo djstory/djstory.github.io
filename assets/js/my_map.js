e90c661fe545b42cb65c64b7d54fb0c568d7e905
Daniel Story <daniel@danieljstory.com>
2019-01-09 20:29:04 -0500

    updating

--- assets/js/my_map.js
+++ assets/js/my_map.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsanN0b3J5IiwiYSI6ImNpbTZnbzNqbDAyNzd0em0zbzd4bWE3NTQifQ.wd6RY9Km7IuzYuKu4Z3Log';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-74.50, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/danieljstory/cjqpqu7160add2qn4w3ahyxks', // stylesheet location
+    center: [7.497612,22.589419], // starting position [lng, lat]
+    zoom: 1 // starting zoom
 });
